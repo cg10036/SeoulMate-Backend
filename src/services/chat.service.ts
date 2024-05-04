df1280a5c087c43745714cd22c6246eca31d5e99
cg10036 <cg10036@gmail.com>
2024-05-04 23:40:11 +0900

    Chore: chatgpt to claude

--- src/services/chat.service.ts
+++ src/services/chat.service.ts
@@ -20,43 +20,37 @@ const chat = async (messages: Message[]) => {
   ];
   let data = await Event.find({
     where: {
-      title: new FindOperator("like", "[%"),
       end: new FindOperator("moreThanOrEqual", new Date()),
     },
   });
 
   let parser = new Parser({ fields });
 
-  let resp = await fetch("https://api.openai.com/v1/chat/completions", {
+  let resp = await fetch("https://api.anthropic.com/v1/messages", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      "X-Api-Key": process.env.ANTHROPIC_API_KEY,
+      "Anthropic-Version": "2023-06-01",
     },
     body: JSON.stringify({
-      model: "gpt-4-turbo",
-      messages: [
-        {
-          role: "system",
-          content:
-            "You are a Seoul Festival Guide. Do your best to guide the user through the given data.\n" +
-            "Answer in the language the user entered, but preferably in Korean.\n" +
-            "Always keep the format {Name}<ID> when outputting the event. Example: {한강 축제}<142345>\n" +
-            `The current date is ${moment
-              .tz("Asia/Seoul")
-              .format("YYYY-MM-DD HH:mm:ss")}`,
-        },
-        {
-          role: "system",
-          content: parser.parse(data),
-        },
-        ...messages,
-      ],
+      model: "claude-3-haiku-20240307",
+      max_tokens: 4096,
+      system:
+        `You are a Seoul Festival Guide affiliated with "SeoulMate" and your name is "SeoulMate AI". Do your best to guide the user through the given data.\n` +
+        "Answer in the language the user entered, but preferably in Korean.\n" +
+        "Always keep the format {FullName}<ID> when outputting the event. FullName MUST NOT BE MODIFIED\n" +
+        `The current date is ${moment
+          .tz("Asia/Seoul")
+          .format("YYYY-MM-DD HH:mm:ss")}\n\n${parser.parse(data)}`,
+      messages: messages,
     }),
   });
   let json = await resp.json();
-
-  return new HttpResponse(200, json.choices[0].message);
+  return new HttpResponse(200, {
+    role: json.role,
+    content: json.content[0].text,
+  });
 };
 
 export default {
